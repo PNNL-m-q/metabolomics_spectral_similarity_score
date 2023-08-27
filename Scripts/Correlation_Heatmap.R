51fdeaefb4243a4f0602908e5a6b86755b1393cd
david.degnan@pnnl.gov <degn400@we45748.pnl.gov>
2023-08-27 14:24:31 -0700

    Score update

--- Scripts/Correlation_Heatmap.R
+++ Scripts/Correlation_Heatmap.R
@@ -9,6 +9,23 @@ Max <- fread("~/Downloads/MaxData.tsv")
 Sum <- fread("~/Downloads/SumData.tsv")
 ScoreMetadata <- fread("~/Git_Repos/metabolomics_spectral_similarity_score/Metadata/Score_Metadata.txt")
 
+# Remove the duplicates
+# Jensen Differences Divergence  --> Jensen Difference Distance
+# Vicis Wave Hadges Distance --> VW1 
+# Vicis Symmetric Chi Squared 1 Distance --> VW2 
+# Vicis Symmetric Chi Squared 2 Distance --> VW3
+# Vicis Symmetric Chi Squared 3 Distance --> VW4
+# Max Symmetric Chi Squared Distance --> VW5
+# Min Symmetric Chi Squared Distance --> VW6
+# Spectral Similarity Score --> Cosine Correlation
+
+ScoreMetadata <- ScoreMetadata %>%
+  filter(Score %in% c("Vicis Wave Hadges Distance", "Vicis-Symmetric X2 1 Distance",
+                      "Vicis-Symmetric X2 2 Distance", "Vicis-Symmetric X2 3 Distance",
+                      "Max Symmetric Chi Squared Distance", "Min Symmetric Chi Squared Distance",
+                      "Spectral Similarity Score", "Kumar Johnson Distance") == FALSE)
+
+
 ##############################
 ## MAX: PEARSON CORRELATION ##
 ##############################
@@ -28,8 +45,8 @@ MaxPearson <- do.call(rbind, lapply(ScoreMetadata$Score, function(Score1) {
 }))
 
 data.frame(
-  Score1 = rep(ScoreMetadata$Score, each = 74),
-  Score2 = rep(ScoreMetadata$Score, 74),
+  Score1 = rep(ScoreMetadata$Score, each = 66),
+  Score2 = rep(ScoreMetadata$Score, 66),
   Pearson = MaxPearson
 ) %>% fwrite("~/Git_Repos/metabolomics_spectral_similarity_score/Data/Max_Pearson.txt", quote = F, row.names = F, sep = "\t")
 
@@ -52,8 +69,8 @@ SumPearson <- do.call(rbind, lapply(ScoreMetadata$Score, function(Score1) {
 }))
 
 data.frame(
-  Score1 = rep(ScoreMetadata$Score, each = 74),
-  Score2 = rep(ScoreMetadata$Score, 74),
+  Score1 = rep(ScoreMetadata$Score, each = 66),
+  Score2 = rep(ScoreMetadata$Score, 66),
   Pearson = SumPearson
 ) %>% fwrite("~/Git_Repos/metabolomics_spectral_similarity_score/Data/Sum_Pearson.txt", quote = F, row.names = F, sep = "\t")
 
